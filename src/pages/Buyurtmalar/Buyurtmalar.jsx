07d4bb67d12cd4ec2d0859262f1c88a3fcd9ab44
Abdulbosit <abdulbosit@Abdulbosits-MacBook-Air.local>
2022-12-01 21:10:31 +0500

    add salom

--- src/pages/Buyurtmalar/Buyurtmalar.jsx
+++ src/pages/Buyurtmalar/Buyurtmalar.jsx
@@ -4,7 +4,82 @@ import './Buyurtmalar.scss'
 function Buyurtmalar() {
   return (
     <div>
-      <h1>Buyurtmalar</h1>
+        <div className="wrapper1">
+          <table className='table'>
+            <thead className='table__thead'>
+              <tr>
+              <th>ID</th>
+              <th>ismi</th>
+              <th>telefon raqami</th>
+              <th>mahsulot nomlari</th>
+              <th>miqdor</th>
+              <th>Qayta aloqa</th>
+              <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>6</td>
+                <td>Jenny Wilson</td>
+                <td>+998 90 123 45 67</td>
+                <td>Ortopedik Eko matras</td>
+                <td>4</td>
+                <td><div className="form-check form-switch">
+                  <input className="form-check-input mody" type="checkbox" id="flexSwitchCheckDefault" defaultChecked/>
+                </div></td>
+                <td>
+                
+                </td>
+              </tr>
+              <tr>
+                <td>7 </td>
+                <td> Jenny Wilson</td>
+                <td>+998 90 123 45 67</td>
+                <td>Ortopedik Eko matras</td>
+                <td>4</td>
+                <td>
+                <div className="form-check form-switch">
+                  <input className="form-check-input mody" type="checkbox" id="flexSwitchCheckDefault"/>
+                </div>
+                </td>
+                <td>
+                  
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Jenny Wilson</td>
+                <td>+998 90 123 45 67</td>
+                <td>Ortopedik Eko matras</td>
+                <td>4</td>
+                <td><div className="form-check form-switch">
+                  <input className="form-check-input mody" type="checkbox" id="flexSwitchCheckDefault" defaultChecked/>
+                </div></td>
+                <td>
+                
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Jenny Wilson</td>
+                <td>+998 90 123 45 67</td>
+                <td>Ortopedik Eko matras</td>
+                <td>4</td>
+                <td>
+                <div className="form-check form-switch">
+                  <input className="form-check-input mody" type="checkbox" id="flexSwitchCheckDefault" defaultChecked/>
+                </div>
+                </td>
+                <td>
+                
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
+
+        </div>
+
     </div>
   )
 }
