426ca93f64a36bc2524829fa508072d1085f28d6
OdilbekDev <desmon.programmer@gmail.com>
2022-12-01 17:01:32 +0500

    maxsulotlar

--- src/pages/Mahsulotlar/Mahsulotlar.jsx
+++ src/pages/Mahsulotlar/Mahsulotlar.jsx
@@ -4,7 +4,6 @@ import './Mahsulotlar.scss'
 function Mahsulotlar() {
   return (
     <div>
-        <h1>Mahsulotlar</h1>
         <div className="wrapper">
           <table className='table'>
             <thead className='table__thead'>
@@ -96,7 +95,7 @@ function Mahsulotlar() {
 
           <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-content modal-add">
               <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">New message</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
