f80e7ee377847f4d3fcbd89bf63abe63312a1b5d
roziqov1011 <roziqov1011@gmail.com>
2022-12-01 16:31:08 +0500

    sadas

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import Header from '../../components/Header/Header'
+import logo from '../../img/admin/logo.svg'
 import './Admin.scss'
 
 function Admin() {
-  return (
-    <div className='Admin'>
-        <Header/>
-        <div className="admin__inner">
-        <div className="admin__bar">
-                <Link to=''>Buyurtmalar</Link>
-                <Link to='customers'>Сustomers</Link>
-                <Link to='toifalar'>Toifalar</Link>
-                <Link to='mahsulotlar'>Mahsulotlar</Link>
-                <Link to='texnologiyalar'>Texnologiyalar</Link>
-                <Link to='manzil'>Manzil</Link>
-        </div>
-        <Outlet/>
-        </div>
-
+return (
+<div className='Admin'>
+  <div className="admin__inner">
+    <div className="admin__bar">
+    <Link className='logo' to=''>
+      <img src={logo} alt="" />
+    </Link>
+      <Link className='admin__link' to=''>Buyurtmalar</Link>
+      <Link className='admin__link' to='customers'>Сustomers</Link>
+      <Link className='admin__link' to='toifalar'>Toifalar</Link>
+      <Link className='admin__link' to='mahsulotlar'>Mahsulotlar</Link>
+      <Link className='admin__link' to='texnologiyalar'>Texnologiyalar</Link>
+      <Link className='admin__link' to='manzil'>Manzil</Link>
     </div>
-  )
+    <div className='Admin__left'>
+      <Header />
+      <Outlet />
+    </div>
+  </div>
+
+</div>
+)
 }
 
 export default Admin
