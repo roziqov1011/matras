9b89017738f9322eb460ddf2e0dbbf543498fe65
Abdulbosit <abdulbosit@Abdulbosits-MacBook-Air.local>
2022-12-01 15:55:25 +0500

    inid

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -2,11 +2,160 @@ import React from 'react'
 import './Customers.scss'
 
 function Customers() {
-  return (
-    <div>
-        <h1>Customers</h1>
+return (
+<div>
+  <div class="form-check form-switch">
+    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+    <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+  </div>
+
+  <div className="admin_page">
+    <div className="admin_pagee">
+      <ul className='admin_list'>
+
+        <li className='admin__item'>
+          <span>Id</span>
+
+        </li>
+        <li className='admin__item'>
+          <span>Sana</span>
+        </li>
+        <li className='admin__item'>
+          <span>TelefonRaqanlar</span>
+        </li>
+        <li className='admin__item'>
+          <span>Qayta Aloqa</span>
+
+        </li>
+      </ul>
     </div>
-  )
+    <div className="line">
+      <ul className='admin_lists'>
+        <li className='admin__itemm'>
+          <span>6</span>
+        </li>
+        <li className='admin__itemn'>
+          <span>Jenny </span>
+        </li>
+        <li className='admin__iteml'>
+          <span>+998 90 123 45 67</span>
+        </li>
+        <li className='admin__itemj'>
+
+
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+          </div>
+
+        </li>
+        <li className='admin__itemi'>
+          <span>4</span>
+        </li>
+        <li className='admin__itema'>
+          <span className='admin__trash'><i class="bi bi-trash"></i></span>
+        </li>
+      </ul>
+
+      <ul className='admin_listss'>
+        <li className='admin__itemm'>
+          <span>6</span>
+        </li>
+        <li className='admin__itemn'>
+          <span>Jenny </span>
+        </li>
+        <li className='admin__iteml'>
+          <span>+998 90 123 45 67</span>
+        </li>
+        <li className='admin__itemj'>
+
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+          </div>
+
+
+        </li>
+        <li className='admin__itemi'>
+          <span>4</span>
+        </li>
+        <li className='admin__itema'>
+          <span><i class="bi bi-trash"></i></span>
+        </li>
+      </ul>
+
+
+      <ul className='admin_listsss'>
+        <li className='admin__itemm'>
+          <span>6</span>
+        </li>
+        <li className='admin__itemn'>
+          <span>Jenny </span>
+        </li>
+        <li className='admin__iteml'>
+          <span>+998 90 123 45 67</span>
+        </li>
+        <li className='admin__itemj'>
+
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+          </div>
+
+
+        </li>
+        <li className='admin__itemi'>
+          <span>4</span>
+        </li>
+        <li className='admin__itema'>
+          <span><i class="bi bi-trash"></i></span>
+        </li>
+      </ul>
+
+      <ul className='admin_listssss'>
+        <li className='admin__itemm'>
+          <span>6</span>
+        </li>
+        <li className='admin__itemn'>
+          <span>Jenny </span>
+        </li>
+        <li className='admin__iteml'>
+          <span>+998 90 123 45 67</span>
+        </li>
+        <li className='admin__itemj'>
+
+
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+          </div>
+
+        </li>
+        <li className='admin__itemi'>
+          <span>4</span>
+        </li>
+        <li className='admin__itema'>
+          <span><i class="bi bi-trash"></i></span>
+        </li>
+      </ul>
+
+
+
+    </div>
+    <div className="line_2">
+
+    </div>
+    <div className="line_3">
+
+    </div>
+    <div className="line_4">
+
+    </div>
+
+
+  </div>
+</div>
+)
 }
 
 export default Customers
