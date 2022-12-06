2b092a7801c967e63f9b307a36ec83d58c553a79
Abdulbosit <abdulbosit@Abdulbosits-MacBook-Air.local>
2022-12-06 11:22:13 +0500

    tr4bnt

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -15,7 +15,7 @@ return (
     <table className='table_nav'>
       <thead className='table_head'>
         
-        <tr>
+        <tr className='trr'>
           <th></th>
           <th>Id</th>
           <th>Sana</th>
@@ -39,77 +39,77 @@ return (
             ))
           } */}
           <td>
-            <div className="form-check form-switch">
+            <div className="form-check form-switch formm">
               <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" defaultChecked />
             </div>
           </td>
             <td>
               <div className="tbody__icon">
-              <i className="bi bi-trash"></i>
+              <i className="bi bi-trash  trashh"></i>
               </div>
             </td>
         </tr>
-        <tr>
+        <tr className='trr'>
           <th></th>
           <td>2</td>
           <td>12:13-12.05.2021</td>
           <td>+998 90 123 45 67</td>
           <td>
-            <div className="form-check form-switch">
+            <div className="form-check form-switch formm">
               <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" defaultChecked />
             </div>
           </td>
             <td>
               <div className="tbody__icon">
-              <i className="bi bi-trash"></i>
+              <i className="bi bi-trash  trashh"></i>
               </div>
             </td>
         </tr>
-        <tr>
+        <tr className='trr'>
           <th></th>
           <td>3</td>
           <td>12:13-12.05.2021</td>
           <td>+998 90 123 45 67</td>
           <td>
-            <div className="form-check form-switch">
+            <div className="form-check form-switch formm">
               <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" defaultChecked />
             </div>
           </td>
             <td>
               <div className="tbody__icon">
-              <i className="bi bi-trash"></i>
+              <i className="bi bi-trash  trashh"></i>
               </div>
             </td>
         </tr>
-        <tr>
+        <tr className='trr'>
           <th></th>
           <td>6</td>
           <td>12:13-12.05.2021</td>
           <td>+998 90 123 45 67</td>
           <td>
-            <div className="form-check form-switch">
+            <div className="form-check form-switch formm">
               <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" defaultChecked />
             </div>
           </td>
             <td>
               <div className="tbody__icon">
-              <i className="bi bi-trash"></i>
+              <i className="bi bi-trash  trashh"></i>
               </div>
             </td>
         </tr>
-        <tr>
+        <tr className='trr'>
           <th></th>
           <td>4</td>
           <td>12:13-12.05.2021</td>
           <td>+998 90 123 45 67</td>
           <td>
-            <div className="form-check form-switch">
+            <div className="form-check form-switch formm">
               <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" defaultChecked />
             </div>
           </td>
             <td>
               <div className="tbody__icon">
-              <i className="bi bi-trash"></i>
+              <i className="bi bi-trash  trashh"></i>
               </div>
             </td>
         </tr>
