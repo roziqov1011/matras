9969bdb499a9419b862e4050f0349c7b6b62a707
suleyman2007 <suleymanahmad2007@gmail.com>
2022-12-09 10:05:29 +0300

    add sulaymon

--- src/components/buyurtma/Buyurtma.jsx
+++ src/components/buyurtma/Buyurtma.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Buyurtma.scss'
+import img from '../../img/img__home/Group.png'
 
 function Buyurtma() {
     return (
@@ -11,7 +12,7 @@ function Buyurtma() {
 
             <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
-                <div class="modal-content" style={{height: '676px'}}>
+                <div class="modal-content" style={{height: '700px'}}>
                 <div class="modal-body buyrtma__modal">
                     <div className="buyurtma__block">
                         <p className="title__b">Buyurtma qilish</p>
@@ -40,12 +41,19 @@ function Buyurtma() {
             </div>
             <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
+              <div class="modal-content" style={{height: '680px'}}>
                 <div class="modal-header">
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                  Hide this modal and show the first with the button below.
+                  <div className="modal2__block2">
+                    <p className="ariza">Arizangiz muvaffaqiyatli yuborildi</p>
+                    <img src={img} alt="" />
+                    <p className="habar">Tez orada operatorlarimiz siz bilan bog’lanishadi</p>
+                    <div class="yubor">
+                          <button class="btn btn-primary " data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Ok</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
