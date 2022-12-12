cb1107bbb9b20cab5a5733784a8bda5b8a351439
Muhammadyusuf <kazievm004@gmail.com>
2022-12-12 08:23:57 +0500

    dfdg

--- src/pages/Texnologiyalar/Texnologiyalar.jsx
+++ src/pages/Texnologiyalar/Texnologiyalar.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import './Texnologiyalar.scss'
 
 function Texnologiyalar() {
   return (
     <div>
-        <h1>Texnologiyalar</h1>
+      <div className="texno">
+        <div className="texno_head">
+        <ul className="list_head">
+          <li className="list_head_item">
+            Nomlari
+          </li>
+          <li className="list_head_item">
+            Matn
+          </li>
+          <li className="list_head_item">
+            Video
+          </li>
+        </ul>
+        </div>
+        <div className="texno_body">
+        <ul className="list_body">
+          <li className="list_body_item">Menory foam</li>
+          <li className="list_body_item">Enim urna... </li>
+          <li className="list_body_item">youtube.com...</li>
+        </ul>
+        </div>
+        </div>
     </div>
   )
 }
 
-export default Texnologiyalar
+export default Texnologiyalar 
