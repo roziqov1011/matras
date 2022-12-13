b67de6f2fa5a664c7146a86502122f5ec8e383f4
Muhammadyusuf <kazievm004@gmail.com>
2022-12-13 08:39:20 +0500

    dfdg

--- src/pages/Texnologiyalar/Texnologiyalar.jsx
+++ src/pages/Texnologiyalar/Texnologiyalar.jsx
@@ -1,52 +1,13 @@
 import React from 'react'
 import './Texnologiyalar.scss'
+import edit from '../../img/edit.png'
 
 function Texnologiyalar() {
   return (
     <div>
       <div className="texno">
-        <div className="texno_head">
-        <ul className="list_head">
-          <li className="list_head_item">
-            Nomlari
-          </li>
-          <li className="list_head_item">
-            Matn
-          </li>
-          <li className="list_head_item">
-            Video
-          </li>
-        </ul>
-        </div>
-        <div className="texno_body">
-        <ul className="list_body">
-          <li className="list_body_item">Menory foam</li>
-          <li className="list_body_item">Enim urna... </li>
-          <li className="list_body_item">youtube.com...</li>
-        </ul>
-        </div>
-        <div className="texno_body">
-        <ul className="list_body">
-          <li className="list_body_item">Menory foam</li>
-          <li className="list_body_item">Enim urna... </li>
-          <li className="list_body_item">youtube.com...</li>
-        </ul>
-        </div>
-        <div className="texno_body">
-        <ul className="list_body">
-          <li className="list_body_item">Menory foam</li>
-          <li className="list_body_item">Enim urna... </li>
-          <li className="list_body_item">youtube.com...</li>
-        </ul>
-        </div>
-        <div className="texno_body">
-        <ul className="list_body">
-          <li className="list_body_item">Menory foam</li>
-          <li className="list_body_item">Enim urna... </li>
-          <li className="list_body_item">youtube.com...</li>
-        </ul>
-        </div>
-        </div>
+        
+      </div>
     </div>
   )
 }
