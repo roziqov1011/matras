872b3ba19db911aaff8ad68dbfa08966cae47c9d
OdilbekDev <desmon.programmer@gmail.com>
2022-12-13 11:46:45 +0500

    maxsulotlar

--- src/pages/Mahsulotlar/Mahsulotlar.jsx
+++ src/pages/Mahsulotlar/Mahsulotlar.jsx
@@ -96,42 +96,42 @@ function Mahsulotlar() {
 
 
           <div className="modal fade" id="maxsulot" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content ">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">New message</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal-dialog">
+              <div className="modal-content ">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">New message</h5>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 
-              </div>
-              <div className="modal-body">
-                <form>
-                  <div className="mb-3">
-                    <label for="recipient-name" className="col-form-label"><img src={input_logo} alt="" /></label>
-                    <input type="file" className="form-control" id="recipient-name" />
-                  </div>
-                  <div className="input__group1">
+                </div>
+                <div className="modal-body">
+                  <form>
                     <div className="mb-3">
-                      <label for="message-text" className="col-form-label">Toifalar</label>
+                      <label for="recipient-name" className="col-form-label"><img src={input_logo} alt="" /></label>
+                      <input type="file" className="form-control" id="recipient-name" />
+                    </div>
+                    <div className="input__group1">
+                      <div className="mb-3">
+                        <label for="message-text" className="col-form-label">Toifalar</label>
+                        <textarea className="form-control" id="message-text"></textarea>
+                      </div>
+                      <div className="mb-3">
+                        <label for="message-text" className="col-form-label">Tovar nomi</label>
+                        <textarea className="form-control" id="message-text"></textarea>
+                      </div>
+                      <div className="mb-3">
+                      <label for="message-text" className="col-form-label">Narxi</label>
                       <textarea className="form-control" id="message-text"></textarea>
                     </div>
-                    <div className="mb-3">
-                      <label for="message-text" className="col-form-label">Tovar nomi</label>
-                      <textarea className="form-control" id="message-text"></textarea>
                     </div>
-                    <div className="mb-3">
-                    <label for="message-text" className="col-form-label">Narxi</label>
-                    <textarea className="form-control" id="message-text"></textarea>
-                  </div>
-                  </div>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Send message</button>
+                  </form>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" className="btn btn-primary">Send message</button>
+                </div>
               </div>
             </div>
-          </div>
-          </div>
+            </div>
         </div>
 
     </div>
