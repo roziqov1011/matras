c19ebdf7525180429f7d037c1d1deae2ee405a38
Muhammadyusuf <kazievm004@gmail.com>
2022-12-13 10:00:59 +0500

    dfdg

--- src/pages/Texnologiyalar/Texnologiyalar.jsx
+++ src/pages/Texnologiyalar/Texnologiyalar.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import './Texnologiyalar.scss'
-import edit from '../../img/edit.png'
 
 function Texnologiyalar() {
   return (
     <div>
       <div className="texno">
-        <ul className="table__head">
+        <ul className="texno__head">
           <li>Nomlari</li>
           <li>Matn</li>
           <li>Video</li>
+        </ul>
+        <ul className="texno__body">
+          <div>
+          <li>Menory foam</li>
+          <li>Enim urna... </li>
+          <li>youtube.com...</li>
+          </div>
+          <div className="images_texno">
+          <li>fef</li>
+          <li>ddd</li>
+          </div>
+        </ul>
+        <ul className="texno__body">
+          <div>
+          <li>Menory foam</li>
+          <li>Enim urna... </li>
+          <li>youtube.com...</li>
+          </div>
+          <div className="images_texno">
+          <li>fef</li>
+          <li>ddd</li>
+          </div>
         </ul>
       </div>
     </div>
